e254d7dadc259639035f36eccd2dcf0e65d050f3
Edgar Olozagaste-Olea <eolozagaste0@gmail.com>
2025-10-16 22:39:18 -0700

    feat: in blog.ts, used foreach loop to create and append each blog element

--- src/blog.ts
+++ src/blog.ts
@@ -13,8 +13,9 @@ const Blogs: Blog[] = [
     date: "10-18-2025",
     description:
       "Read about how my love for computers began at a young age, went into hiding when the world needed it most, and came back 5 years later...",
-    image: "path",
-    imageAlt: "TBD",
+    image: "/images/compsci_blog.jpg",
+    imageAlt:
+      "A picture of my workstation showing a computer, engineering project, and Monster energy drink.",
     slug: "path",
   },
   {
@@ -22,10 +23,28 @@ const Blogs: Blog[] = [
     date: "10-18-2025",
     description:
       "Read about how I, a young man who swore he had no creative side of his brain, grew a love for one of the most creativity-requiring hobbies; guitar playing.",
-    image: "path",
-    imageAlt: "TBD",
+    image: "/images/guitar_blog.jpg",
+    imageAlt: "A picture of me playing a baby guitar in guitar center",
     slug: "path",
   },
 ];
 
-function 
+document.addEventListener("DOMContentLoaded", () => {
+  Blogs.forEach((blog) => {
+    const blogContainer = document.querySelector("#blog-container");
+
+    const blogPost = document.createElement("div");
+    blogPost
+    const title = document.createElement("h1");
+    const image = document.createElement("img");
+    const description = document.createElement("p");
+
+    title.textContent = blog.title;
+    image.src = blog.image;
+    image.alt = blog.imageAlt;
+    description.textContent = blog.description;
+
+    blogPost.append(title, image, description);
+    if (blogContainer) blogContainer.appendChild(blogPost); // Thanks to Emmy in the Discord for this line of code, without checking first if blogContainer exists, error will pop up.
+  });
+});
