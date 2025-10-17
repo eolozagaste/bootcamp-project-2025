66b79836eaf649ee7a3bc14472a8becfbda8369d
Edgar Olozagaste-Olea <eolozagaste0@gmail.com>
2025-10-16 21:41:56 -0700

    cleanup: took startpack files out of old dir, updated file paths

--- src/blog.ts
+++ src/blog.ts
@@ -5,13 +5,14 @@ type Blog = {
   image: string;
   imageAlt: string;
   slug: string;
-}
+};
 
 const Blogs: Blog[] = [
   {
     title: "My Journey into Comp Sci.",
     date: "10-18-2025",
-    description: "Read about how my love for computers began at a young age, went into hiding when the world needed it most, and came back 5 years later...",
+    description:
+      "Read about how my love for computers began at a young age, went into hiding when the world needed it most, and came back 5 years later...",
     image: "path",
     imageAlt: "TBD",
     slug: "path",
@@ -19,10 +20,12 @@ const Blogs: Blog[] = [
   {
     title: "FINALLY! A Creative Hobby: Guitar.",
     date: "10-18-2025",
-    description: "Read about how I, a young man who swore he had no creative side of his brain, grew a love for one of the most creativity-requiring hobbies; guitar playing.",
+    description:
+      "Read about how I, a young man who swore he had no creative side of his brain, grew a love for one of the most creativity-requiring hobbies; guitar playing.",
     image: "path",
     imageAlt: "TBD",
     slug: "path",
-  }
-]
+  },
+];
 
+function 
